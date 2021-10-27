4b84f7bf4591ba7cb24513b78dc5e0476d79c40c
vazha <phankvelashvili.mail@gmail.com>
2021-10-27 14:46:09 +0400

    change js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,23 @@
-var BuisnessNews=document.querySelectorAll(".TopNewsesContainerLeftContent");
-var BCount=0;
-let TopNewsesContainerLeft=document.querySelector(".TopNewsesContainerLeft")
-
-TopNewsesContainerLeft.addEventListener("click",function(){
-if(BCount<BuisnessNews.length){
-    
-    for(var i=0;i<BuisnessNews.length;i++){
-        BCount++;
-        console.log(BuisnessNews[i])
-        
+var SlidesItems=document.querySelectorAll(".TopNewsesContainerLeftContent");
+let currentindex=0;
+var B=document.querySelector(".btn");
+function Sld() {
+    for(var i=0;i<SlidesItems.length;i++){
+        SlidesItems[i].style.display="none";
     }
+    SlidesItems[currentindex].style.display="block";
+
 }
+function right() {
+    if(currentindex==SlidesItems.length-1){
+    
+        currentindex++;
+    }else{
+        currentindex=0;
+    }
+    Sld()
+}
+B.addEventListener('click',right());
+setInterval(function () {
+    B
 })
