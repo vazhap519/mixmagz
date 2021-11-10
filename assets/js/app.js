e2e168d231799503a28d0e3754708feb31cc6459
vazha <phankvelashvili.mail@gmail.com>
2021-11-10 13:01:32 +0400

    add slide

--- assets/js/app.js
+++ assets/js/app.js
@@ -77,31 +77,24 @@ function HContLenUpdate(){
 }
 setInterval(HContLenUpdate,1000)
 
-$(document).ready(function() {
-    $('#autoWidth').lightSlider({
-        item:4,
-        loop:false,
-        slideMove:2,
-        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
-        speed:600,
-        responsive : [
-            {
-                breakpoint:800,
-                settings: {
-                    item:3,
-                    slideMove:1,
-                    slideMargin:6,
-                  }
-            },
-            {
-                breakpoint:480,
-                settings: {
-                    item:2,
-                    slideMove:1
-                  }
-            }
-        ]
-    });  
-  });
-
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:true
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:true
+        }
+    }
+})
     
