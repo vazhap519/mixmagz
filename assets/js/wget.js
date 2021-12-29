f5dd07137fd0829e490a62b906fa92a3e9f1e8eb
vazha <phankvelashvili.mail@gmail.com>
2021-12-29 04:07:54 -0800

    ADD CATEGORY PAGE CAROUSELL

--- assets/js/wget.js
+++ assets/js/wget.js
@@ -3,12 +3,10 @@ let TopStoryWgetContent=document.querySelectorAll(".TopStoryWgetContentPosts");
 let TopStoryWgetLeft=document.querySelector(".TopStoryWgetButtonsLeft");
 let TopStoryWgetRight=document.querySelector(".TopStoryWgetButtonsRight");
 let TopStoryWgetCount=0;
-
 function TopStoryWgetContentUpdate(){
     for(var i=0;i<TopStoryWgetContent.length;i++){
         TopStoryWgetContent[i].style.display="none"
     }
-
 }
 function TopStoryWgetContentUpdateRight(){
     TopStoryWgetContentUpdate()
@@ -19,7 +17,6 @@ function TopStoryWgetContentUpdateRight(){
     }
     TopStoryWgetContent[TopStoryWgetCount].style.display="block";
 
-   
 }
 
 function TopStoryWgetContentUpdateLeft(){
@@ -30,12 +27,28 @@ function TopStoryWgetContentUpdateLeft(){
     TopStoryWgetCount--
     TopStoryWgetContent[TopStoryWgetCount].style.display="block";
     
-    
-
-   
 }
 
 TopStoryWgetRight.addEventListener("click",TopStoryWgetContentUpdateRight);
 TopStoryWgetLeft.addEventListener('click',TopStoryWgetContentUpdateLeft);
 
 setInterval(TopStoryWgetContentUpdateRight,1000)
+//END TOP STORY CROUSEL
+let index=0;
+let CategoryPageCarouselItems=document.querySelectorAll(".category_page_Carousell_content");
+function CategoryPageCarousel(){
+    for(var index=0;index<CategoryPageCarouselItems.length;index++){
+        CategoryPageCarouselItems[index].style.display="none";
+    }
+}
+function CategoryPageCarouselUpdate(){
+    CategoryPageCarousel()
+    index++;
+    if(index>CategoryPageCarouselItems.length-1){
+        index=0
+    }
+    CategoryPageCarouselItems[index].style.display="block";
+}
+
+setInterval(CategoryPageCarouselUpdate,1000)
+//END CATEGORY PAGE  CROUSEL
