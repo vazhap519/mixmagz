07f505721414af904b2fb17ddcfd42c96d970bee
vazha pankvelashvili <phankvelashvili.mail@gmail.com>
2022-01-05 09:02:45 -0800

    start form validate

--- assets/js/wget.js
+++ assets/js/wget.js
@@ -37,70 +37,37 @@ setInterval(TopStoryWgetContentUpdateRight,1000)
 
 
 
+
 function ContactValid(){
     let ContactForm=document.querySelector(".ContactForm");
-    let NameInput=ContactForm.querySelector("input[name='name']");
-    let Email=ContactForm.querySelector("input[name='email']");
-    let Message=ContactForm.querySelector("input[name='Message']");
-    let Phone=ContactForm.querySelector("input[name='tel']");
-  
-    ContactForm.addEventListener('submit',(e)=>{
-        e.preventDefault;
-        if(NameInput.value==""){
-          var span=document.createElement("span");
-          span.classList.add('error nameError');
-          span.textContent="გთხოვთ შეიყვანოთ სახელი";
-          span.append( form-group-name);
-        }else{
-            var span=document.createElement("span");
-            span.classList.add('success namesucsess');
-            span.textContent="გთხოვთ გააგრძელოთ ";
-        }
+let NameInput=document.querySelector(".NameInp");
+let Email=document.getElementsByClassName("EmailInp");
+let Message=document.getElementsByClassName("MessageInp");
+let Phone=document.getElementsByClassName("TelInp")
+ let contactBtn=document.getElementsByClassName("contactBtn");
+let sucsses=document.querySelector(".sucsses");
+let error=document.querySelector(".error");
 
-
-
-
-        if(Email.value==""){
-            var span=document.createElement("span");
-            span.classList.add('error Emailrror');
-            span.textContent="გთხოვთ შეიყვანოთ იმეილი";
-            span.append(Email);
-          }else{
-              var span=document.createElement("span");
-              span.classList.add('success Emailsucsess');
-              span.textContent="გთხოვთ გააგრძელოთ ";
-          }
-
-
-
-          
-        if(Message.value==""){
-            var span=document.createElement("span");
-            span.classList.add('error MessageError');
-            span.textContent="გთხოვთ შეიყვანოთ შეტყობინება";
-            span.append(Email);
-          }else{
-              var span=document.createElement("span");
-              span.classList.add('success Messagesucsess');
-              span.textContent="გთხოვთ გააგრძელოთ ";
-          }
-
-
-
-
-          if(Phone.value==""){
-            var span=document.createElement("span");
-            span.classList.add('error PhoneError');
-            span.textContent="გთხოვთ შეიყვანოთ ტელეფონი";
-            span.append(Email);
-          }else{
-              var span=document.createElement("span");
-              span.classList.add('success Phonesucsess');
-              span.textContent="გთხოვთ გააგრძელოთ ";
-          }
-    })
-
-
-    
+ContactForm.addEventListener("submit",function(e){
+e.preventDefault();
+if(NameInput.value.trim()==="" || NameInput.value.length<3 ){
+    error.style.display="block";
+    NameInput.style.border="1px solid red";
+}else{
+  sucsses.style.display="block";
+    NameInput.style.border="1px solid green";
+    error.style.display="none";
 }
+
+
+
+
+
+
+
+})
+
+
+}
+   
 ContactValid()
