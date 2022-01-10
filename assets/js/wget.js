4e9a5dce751c634948a9dd52c19ebe01550e3c4e
vazha pankvelashvili <phankvelashvili.mail@gmail.com>
2022-01-10 05:21:20 -0800

    contact form validation

--- assets/js/wget.js
+++ assets/js/wget.js
@@ -36,38 +36,98 @@ setInterval(TopStoryWgetContentUpdateRight,1000)
 //END TOP STORY CROUSEL
 
 
-
-
-function ContactValid(){
-    let ContactForm=document.querySelector(".ContactForm");
+let ContactForm=document.querySelector(".ContactForm");
 let NameInput=document.querySelector(".NameInp");
-let Email=document.getElementsByClassName("EmailInp");
-let Message=document.getElementsByClassName("MessageInp");
-let Phone=document.getElementsByClassName("TelInp")
- let contactBtn=document.getElementsByClassName("contactBtn");
-let sucsses=document.querySelector(".sucsses");
-let error=document.querySelector(".error");
-
-ContactForm.addEventListener("submit",function(e){
-e.preventDefault();
-if(NameInput.value.trim()==="" || NameInput.value.length<3 ){
-    error.style.display="block";
-    NameInput.style.border="1px solid red";
-}else{
-  sucsses.style.display="block";
-    NameInput.style.border="1px solid green";
-    error.style.display="none";
-}
+let Email=document.querySelector(".EmailInp");
+let Message=document.querySelector(".MessageInp");
+let Phone=document.querySelector(".TelInp")
 
 
-
-
-
-
-
+ContactForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    validateForm()
 })
 
 
-}
+
+function validateForm(){
+    //Name
+    if(NameInput.value.trim()==""){
+        setError(NameInput ," გთხოვთ შეავსოთ ველი");
+    }else if(NameInput.value.trim().length<3){
+        setError(NameInput ,"მინიმუალური სიმბოლოების რაოდენობა 3");
+    }else{
+        setSuccsess(NameInput,'გთხოვთ გააგრძელოთ')
+    }
+    //Email
+
+    if(Email.value.trim()==""){
+        setError(Email ," გთხოვთ შეავსოთ ველი");
+    }else if(isEmailValid(Email.value)){
+        setSuccsess(Email,"გთხოვთ გააგრძელოთ");
+    }else{
+        setError(Email,"გადაამოწმე იმეილი")
+    }
+
+  
+let regex = /^[\d,\s,\+,\-]{5,20}/;
+    //phone
+   if(Phone.value==""){
+    setError(Phone,"გადაამოწმე ნომერი")  
+   }else if(Phone.value.match(regex)){
+    setSuccsess(Phone,"გთხოვთ გააგრძელოთ");
+   }
+   else{
+    setError(Phone,"ნომერი არასწორია" )  
    
-ContactValid()
+   }
+
+     //message
+
+  if(Message.value.trim()==""){
+    setError(Message ," შეტრობინება ცარიელია");
+  }else if(Message.value.trim().length<10){
+    setError(Message ," შეტრობინება 10 სიმბოლოზე ნაკლებია");
+  }
+else{
+    setSuccsess(Message,"გთხოვთ გააგრძელოთ");
+}
+
+}
+
+
+function setError(element, Errormessage){
+    const parent=element.parentElement;
+    if(parent.classList.contains("sucsses")){
+
+        parent.classList.remove("sucsses");
+    }
+    parent.classList.add('error')
+    const smaall=parent.querySelector("small")
+    smaall.textContent=Errormessage;
+}
+
+
+
+function setSuccsess(element,succsessMesage){
+    const parent=element.parentElement;
+    if(parent.classList.contains("error")){
+        parent.classList.remove("error")
+    }
+    parent.classList.add("sucsses");
+    const small=parent.querySelector("small");
+    small.textContent=succsessMesage;
+
+}
+
+
+function isEmailValid(email){
+    const reg =/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    return reg.test(email);
+}
+
+function isPhoneValid(Phone){
+    const PhoneReg= /^[\d,\s,\+,\-]{5,20}/;
+    return PhoneReg.test(Phone)
+}
